d2c8a4827958f24452f641425f76720c004bb184
GengroHirano <yuusyanokuseninamaikida.or2@gmail.com>
2013-06-08 01:10:44 +0900

    暇つぶしに

---
+++ FIZZBUZZ.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+x=0
+echo "数字を入力してください"
+read inRead
+if [ $inRead ] ; then
+	while [ $x -ne $inRead ]
+	do
+		x=`expr $x + 1`
+		flag=0
+		message=$x
+		if [ `expr $x % 3` -eq 0 ] ; then
+			flag=`expr $flag + 1`
+		fi
+		if [ `expr $x % 5` -eq 0 ] ; then
+			flag=`expr $flag + 2`
+		fi
+		case $flag in
+		1 ) message="FIZZ" ;;
+		2 ) message="BUZZ" ;;
+		3 ) message="FIZZBUZZ" ;;
+		* ) message="$x" ;;
+		esac
+		echo "${message}"
+	done
+else
+	echo "数字を入力してって言ったじゃないか・・・"
+	fi
